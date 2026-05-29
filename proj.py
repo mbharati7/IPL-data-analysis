74197f4fc2cb9a367a8798276e3d0e6a30b6604e
Bharati Meher <meherbharati447@gmail.com>
2026-05-29 11:02:23 +0530

    Delete proj.py

--- proj.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Load the full dataset
-df = pd.read_csv("IPL.csv")
-
-# See what you're working with
-print(df.shape)        # how many rows and columns
-print(df.columns.tolist())   # column names
-print(df.head())       # first 5 rows
